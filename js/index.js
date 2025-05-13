57e222639a305af39116f51834f1fa297020ee83
Youssef Hossam <155332489+YoussefHosssam@users.noreply.github.com>
2025-05-13 19:45:17 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-let loggedIn = JSON.parse(localStorage.getItem("loggedInUser")) || "" ;
-let nav = document.querySelector(".ul") ;
-let admin = document.createElement("li") ;
-if (loggedIn.role === "admin"){
-window.location.href = "admin/dashboard.html" ;
-}
-else if (loggedIn.role === "user"){
-    window.location.href = "app/dashboard.html" ;
-}
